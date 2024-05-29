29e4d64e9a41cd46837dd0fb8a928327a56cdecc
Mohammad Abbass <mhmd.abbass.90@gmail.com>
2024-05-29 11:57:31 +0300

    refactor the plugin into 2 plugins

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect, useRef } from 'react';
 import * as esbuild from 'esbuild-wasm';
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
+import { fetchPlugin } from './plugins/fetch-plugin';
 
 const App = () => {
   const [input, setInput] = useState('');
@@ -30,7 +31,7 @@ const App = () => {
         entryPoints: ['index.js'],
         bundle: true,
         write: false,
-        plugins: [unpkgPathPlugin()],
+        plugins: [unpkgPathPlugin(), fetchPlugin(input)],
         define: {
           'process.env.NODE_ENV': '"production"',
           global: 'window',
