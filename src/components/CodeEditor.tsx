4288ce0e79500d0a588c6b0c4f6f1d511b0bd7f7
Mohammad Abbass <mhmd.abbass.90@gmail.com>
2024-05-30 15:27:43 +0300

    add some configrations to the editor

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -1,7 +1,24 @@
 import Editor from '@monaco-editor/react';
 
 const codeEditor = () => {
-  return <Editor height={'90vh'} language='javascript' />;
+  return (
+    <Editor
+      height={'90vh'}
+      language='javascript'
+      theme='vs-dark'
+      options={{
+        wordWrap: 'on',
+        showUnused: false,
+        minimap: { enabled: false },
+        folding: false,
+        lineNumbersMinChars: 3,
+        fontSize: 16,
+        scrollBeyondLastLine: false,
+        fontFamily: 'Fira Code',
+        automaticLayout: true,
+      }}
+    />
+  );
 };
 
 export default codeEditor;
